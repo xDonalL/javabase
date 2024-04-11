04e8bff962d387bcced593923cd7ccd86c794894
Yuriy Subbotin <Zencer1335@gmail.com>
2024-04-11 22:31:20 +0300

    added class Config

---
+++ src/come/urise/webapp/Config.java
@@ -0,0 +1,40 @@
+package come.urise.webapp;
+
+import come.urise.webapp.storage.SqlStorage;
+import come.urise.webapp.storage.Storage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+    private static final File PROPS = new File("config\\resumes.properties");
+    private static final Config INSTANCE = new Config();
+
+    private final String storageDir;
+    private final Storage storage;
+
+    public static Config get() {
+        return INSTANCE;
+    }
+
+    private Config() {
+        try (InputStream is = new FileInputStream(PROPS)) {
+            Properties props = new Properties();
+            props.load(is);
+            storageDir = props.getProperty("storage.dir");
+            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+        }
+    }
+
+    public String getStorageDir() {
+        return storageDir;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+}
