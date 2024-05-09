52bbffdbf3c4a102461bff5fb04b5cfe9f2369ab
Yuriy Subbotin <Zencer1335@gmail.com>
2024-05-09 14:19:15 +0300

    added setFullName

--- src/come/urise/webapp/model/Resume.java
+++ src/come/urise/webapp/model/Resume.java
@@ -82,6 +82,10 @@ public class Resume implements Comparable<Resume>, Serializable {
         return fullName;
     }
 
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
     public String getUuid() {
         return uuid;
     }
